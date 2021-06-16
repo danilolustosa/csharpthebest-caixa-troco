9a8419f28316e686146e1eb9c25945f11bb675ca
Danilo Lustosa <danilolustosa@gmail.com>
2021-06-15 23:09:55 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do produto:");
+            string input = Console.ReadLine();
+            double valorProduto = Convert.ToDouble(input);
+
+            string inputPagamento = "100";
+
+            while (inputPagamento != "")
+            {
+                Console.WriteLine("Digite o valor pago pelo produto:");
+                inputPagamento = Console.ReadLine();
+
+                double valorPagamento = 0;
+
+                if (inputPagamento != "")
+                {
+                    valorPagamento = Convert.ToDouble(inputPagamento);
+
+                    if ((valorPagamento - valorProduto) > 0)
+                    {
+                        Console.WriteLine("O valor do troco é: " + (valorPagamento - valorProduto));
+                    }
+                }                     
+            }
+
+            
+        }
+    }
+}
